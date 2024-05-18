6a083a25fe192076511df7babf63a944b224fabf
Karol-Ramirez03 <karol03112006@gmail.com>
2024-05-18 18:00:39 -0500

    usermodel

---
+++ src/users/models/user.js
@@ -0,0 +1,15 @@
+//aqui se va a realizar el modelos de datos
+export class User {
+
+
+    /**
+     * 
+     * @param {Like<User>} userDataLike 
+     */
+    constructor({ id,  }) {
+        this.id       = id;
+    
+    }
+    
+
+}
